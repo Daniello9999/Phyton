23023c52afaf5d0867ee2a8e9ccc25d389348f0e
Daniello9999 <poplonski.daniel@gmail.com>
2018-10-06 16:41:54 +0200

    pierwszy commit

--- Zjazd_1/zad_11.py
+++ Zjazd_1/zad_11.py
@@ -1,5 +1,13 @@
 x = int(input(podaj pozycję gracza x:))
 y = int(input(podaj pozycję gracza y:))
 
-if x < 0 or x > 95 or y > 0 or y > 95:
-    print(prawa górna krawendz)
+if x > 100 or y > 100 or x < 0 or y < 0:
+    print("poza planszą")
+elif x > 90 and y > 90:
+    print ("prawy górny róg")
+elif x > 90 and y < 10:
+    print ("prawy dolny róg")
+elif x < 10 and y < 10:
+    print ("lewy dolny róg")
+elif x < 10 and y > 90:
+    print("lewy górny róg")
